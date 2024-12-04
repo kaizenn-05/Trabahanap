22fdfe45abe6a2ddfa33797e64e2181cf97842dc
KAIZEN <yoinketoink@gmail.com>
2024-12-04 18:24:01 +0800

    changed

--- resources/views/components/home page/footer.blade.php
+++ resources/views/components/home page/footer.blade.php
@@ -9,7 +9,8 @@
                             <div class="footer-tittle">
                                 <h4>About Us</h4>
                                 <div class="footer-pera">
-                                    <p>JobMatch is a platform that connects job seekers and employers in the provincial job market, making it easier to find the right opportunities and talent.</p>
+                                    <p>JobMatch is a platform that connects job seekers and employers in the provincial
+                                        job market, making it easier to find the right opportunities and talent.</p>
                                 </div>
                             </div>
                         </div>
@@ -22,10 +23,10 @@
                             <h4>Contact Info</h4>
                             <ul>
                                 <li>
-                                    <p>Address :Aicia Z,S,P.</p>
+                                    <p>Address :Sindangan, Z.D.N.</p>
                                 </li>
                                 <li><a href="#">Phone : +9xxxxxxx</a></li>
-                                <li><a href="#">Email : Jane@gmail.com</a></li>
+                                <li><a href="#">Email : yoinketoink@gmail.com</a></li>
                             </ul>
                         </div>
 
@@ -50,21 +51,21 @@
                         <div class="footer-tittle">
                             <h4>Newsletter</h4>
                             <div class="footer-pera footer-pera2">
-                                <p>Heaven fruitful doesn't over lesser in days. Appear creeping.</p>
+                                <p>Something Newsletter here.</p>
                             </div>
                             <!-- Form -->
                             <div class="footer-form">
                                 <div id="mc_embed_signup">
                                     <form target="_blank"
-                                          action="https://spondonit.us12.list-manage.com/subscribe/post?u=1462626880ade1ac87bd9c93a&amp;id=92a4423d01"
-                                          method="get" class="subscribe_form relative mail_part" novalidate="true">
+                                        action="https://spondonit.us12.list-manage.com/subscribe/post?u=1462626880ade1ac87bd9c93a&amp;id=92a4423d01"
+                                        method="get" class="subscribe_form relative mail_part" novalidate="true">
                                         <input type="email" name="EMAIL" id="newsletter-form-email"
-                                               placeholder="Email Address" class="placeholder hide-on-focus"
-                                               onfocus="this.placeholder = ''"
-                                               onblur="this.placeholder = ' Email Address '">
+                                            placeholder="Email Address" class="placeholder hide-on-focus"
+                                            onfocus="this.placeholder = ''"
+                                            onblur="this.placeholder = ' Email Address '">
                                         <div class="form-icon">
                                             <button type="submit" name="submit" id="newsletter-submit"
-                                                    class="email_icon newsletter-submit button-contactForm"><img
+                                                class="email_icon newsletter-submit button-contactForm"><img
                                                     src="assets/img/icon/form.png" alt=""></button>
                                         </div>
                                         <div class="mt-10 info"></div>
@@ -80,7 +81,8 @@
                 <div class="col-xl-3 col-lg-3 col-md-4 col-sm-6">
                     <!-- logo -->
                     <div class="footer-logo mb-20">
-                        <a href="index.html"><img src="{{asset('frontend')}}/assets/img/logo/logov4.png" alt="" style="width: 199px"></a>
+                        <a href="index.html"><img src="{{ asset('frontend') }}/assets/img/logo/logov4.png"
+                                alt="" style="width: 199px"></a>
                     </div>
                 </div>
                 <div class="col-xl-3 col-lg-3 col-md-4 col-sm-5">
@@ -114,9 +116,10 @@
                         <div class="footer-copy-right">
                             <p>
                                 Copyright ©
-                                <script>document.write(new Date().getFullYear());</script>
-                                 Project by Jane <a
-                                    href="" ></a>
+                                <script>
+                                    document.write(new Date().getFullYear());
+                                </script>
+                                Project by 3rd-year BSIT <a href=""></a>
 
                             </p>
                         </div>
